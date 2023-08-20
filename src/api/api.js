490a80578667e0d3c1475c7c21a30612f7659754
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-08-20 14:52:54 +0200

    fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '38889526-086820321c5fcbdbccd359080';
+// const API_KEY = '38889526-086820321c5fcbdbccd359080'; 
 
- async function onGetImage(page = 1, value) {
+const API_KEY = '38613444-415ea31856756f0ffc19b62a0'; 
+
+
+
+async function onGetImage(page = 1, value) {
+    console.log(axios.defaults);
          axios.defaults.params = {
              key: API_KEY,
              q: value,
@@ -13,7 +18,10 @@ const API_KEY = '38889526-086820321c5fcbdbccd359080';
              per_page: 40,
          };
          const responce = await axios.get(BASE_URL);
-         return responce.data;
+     return responce.data;
+     
+     
 }
  
 export { onGetImage } 
+
